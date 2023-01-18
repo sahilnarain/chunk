c33206da36f908d0debc9d3492f15f8a758565f0
Sahil Narain <sahil.narain@gmail.com>
2023-01-18 17:40:16 +0530

    Recursive dir reading

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const gfs = require('get-folder-size');
+const ls = require('recursive-readdir');
 
 const config = {
   DIR: '.',
@@ -16,30 +17,37 @@ const getSize = gfs(config.DIR, (err, result) => {
 
   console.log('Folder size: ', size);
   console.log('Limit: ', config.LIMIT_SIZE);
+  console.log('-----');
 
   let dirCounter = 0;
   let dirSizeCounter = 0;
 
-  let files = fs.readdirSync(config.DIR, {recursive: true});
-  if (!fs.existsSync(`${config.OUTDIR}/${dirCounter}`)) {
-    console.log(`${config.OUTDIR}/${dirCounter}`);
-    fs.mkdirSync(`${config.OUTDIR}/${dirCounter}`);
-  }
-
-  files.forEach((file) => {
-    console.log(file, dirSizeCounter);
-    let fileSize = fs.statSync(`${config.DIR}/${file}`).size / (1024 * 1024);
-    if (dirSizeCounter + fileSize < config.LIMIT_SIZE) {
-      dirSizeCounter += fileSize;
-      fs.copyFileSync(`${config.DIR}/${file}`, `${config.OUTDIR}/${dirCounter}/${file}`);
-    } else {
-      dirSizeCounter = 0;
-      dirCounter++;
+  // let files = fs.readdirSync(config.DIR, {recursive: true});
+  let files = ls(config.DIR, (err, files) => {
+    if (!fs.existsSync(`${config.OUTDIR}/${dirCounter}`)) {
+      console.log();
       console.log(`${config.OUTDIR}/${dirCounter}`);
       fs.mkdirSync(`${config.OUTDIR}/${dirCounter}`);
-
-      dirSizeCounter += fileSize;
-      fs.copyFileSync(`${config.DIR}/${file}`, `${config.OUTDIR}/${dirCounter}/${file}`);
     }
+
+    files.forEach((file) => {
+      file = file.replace(config.DIR + '/', '');
+      let fileSize = fs.statSync(`${config.DIR}/${file}`).size / (1024 * 1024);
+      console.log(file, dirSizeCounter);
+
+      if (dirSizeCounter + fileSize < config.LIMIT_SIZE) {
+        dirSizeCounter += fileSize;
+        fs.copyFileSync(`${config.DIR}/${file}`, `${config.OUTDIR}/${dirCounter}/${file.split('/')[file.split('/').length - 1]}`);
+      } else {
+        dirSizeCounter = 0;
+        dirCounter++;
+        console.log();
+        console.log(`${config.OUTDIR}/${dirCounter}`);
+        fs.mkdirSync(`${config.OUTDIR}/${dirCounter}`);
+
+        dirSizeCounter += fileSize;
+        fs.copyFileSync(`${config.DIR}/${file}`, `${config.OUTDIR}/${dirCounter}/${file.split('/')[file.split('/').length - 1]}`);
+      }
+    });
   });
 });
